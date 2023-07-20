412bf1181234ef1d7f3c2c5f9480048a61d46c55
jeongeun <jeun1015@cowave.kr>
2023-07-20 16:59:12 +0900

    RateDiscountPolicy 개발

---
+++ src/main/java/hello/javaspringbasic/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.javaspringbasic.discount;
+
+import hello.javaspringbasic.member.Grade;
+import hello.javaspringbasic.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
